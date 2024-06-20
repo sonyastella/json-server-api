eb8f1dd5576d29aa246212bdbed2d5b3c6739c14
sonyastella <161102066+sonyastella@users.noreply.github.com>
2024-06-19 22:17:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('db.json'); // Path to your JSON file
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
-server.use(router);
-server.listen(5000, () => {
-  console.log('JSON Server is running');
+server.use('/api', router);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
